fca402923dcb69aa3f3e18af130d13c3dc0e3bdb
Artem Peshkov <a.o.peshkov@yandex.ru>
2024-04-14 22:13:33 +0500

    ttask

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // const readline = require('readline');
 
 // const rl = readline.createInterface({
-//     input: process.stdin
+//     input: process.stdin,
+//     output: process.stdout,
 // });
 
 // let numbers
@@ -11,7 +12,22 @@
 //     const [a, b] = numbers
 //     let result = +a + +b;
 //     process.stdout.write(result.toString());
+//     console.log(result)
 // });
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+let numbers = 0;
+rl.on("line", line => {
+	numbers += +line;
+}).on("close", () => {
+	process.stdout.write(numbers.toString());
+});
 
 //   const fs = require('fs');
 //   let str = fs.readFileSync('input.txt', 'utf8')
@@ -22,35 +38,34 @@
 // const result = A + B;
 // fs.writeFileSync("output.txt", result,  "ascii")
 
-const fs = require("fs");
-let str = fs.readFileSync("./input.txt", "utf8");
-str = str.split('\n')
-const [jewels, stones] = str;
-let result = 0;
-for (let i = 0; i < stones.length; i++) {
-	if (jewels.includes(stones.charAt(i))) {
-		result++;
-	}
-}
-fs.writeFileSync("output.txt", result.toString(), "ascii");
+// const fs = require("fs");
+// let str = fs.readFileSync("./input.txt", "utf8");
+// str = str.split('\n')
+// const [jewels, stones] = str;
+// let result = 0;
+// for (let i = 0; i < stones.length; i++) {
+// 	if (jewels.includes(stones.charAt(i))) {
+// 		result++;
+// 	}
+// }
+// fs.writeFileSync("output.txt", result.toString(), "ascii");
 
+// const readline = require('readline');
 
-const readline = require('readline');
- 
-const rl = readline.createInterface({
-    input: process.stdin
-});
- 
-let lines = [];
-rl.on('line', (line) => {
-    lines.push(line);
-}).on('close', () => {
-    const [jewels, stones] = lines
-    let result = 0;
-    for (let i = 0; i < stones.length; i++) {
-        if (jewels.includes(stones.charAt(i))) {
-            ++result;
-        }
-    }
-    process.stdout.write(result.toString());
-});
+// const rl = readline.createInterface({
+//     input: process.stdin
+// });
+
+// let lines = [];
+// rl.on('line', (line) => {
+//     lines.push(line);
+// }).on('close', () => {
+//     const [jewels, stones] = lines
+//     let result = 0;
+//     for (let i = 0; i < stones.length; i++) {
+//         if (jewels.includes(stones.charAt(i))) {
+//             ++result;
+//         }
+//     }
+//     process.stdout.write(result.toString());
+// });
